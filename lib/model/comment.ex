70230c0408990ae0bbdf86a0cf7d882220b83255
Daniils Petrovs <dpetrovs@evolution.com>
2021-09-12 22:27:01 +0200

    Define comment model struct

--- lib/model/comment.ex
+++ lib/model/comment.ex
@@ -1,12 +1,27 @@
 defmodule Holodex.Model.Comment do
   @moduledoc false
 
+  defstruct [:comment_key, :video_id, :message]
+
   @typedoc """
   `Comment` resource type definition.
   """
-  @type t() :: %{
+  @type t() :: %__MODULE__{
           comment_key: String.t(),
           video_id: String.t(),
           message: String.t()
         }
+
+  @spec build(%{
+          (comment_key :: String.t()) => String.t(),
+          (video_id :: String.t()) => String.t(),
+          (message :: String.t()) => String.t()
+        }) :: __MODULE__.t()
+  def build(map) do
+    %__MODULE__{
+      comment_key: map["comment_key"],
+      video_id: map["video_id"],
+      message: map["message"]
+    }
+  end
 end
